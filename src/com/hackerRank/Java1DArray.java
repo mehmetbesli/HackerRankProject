53c7440055b965148af8238a0f2456c2072892d9
MehmetBesli <beslimhmht@gmail.com>
2020-05-03 15:37:35 +0300

    HackerRank Project

---
+++ src/com/hackerRank/Java1DArray.java
@@ -0,0 +1,24 @@
+package com.hackerRank;
+
+import java.util.Scanner;
+
+/**
+ * created by MehmetBesli on 05.2020
+ */
+public class Java1DArray {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+
+        int [] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i]=scan.nextInt();
+        }
+
+        scan.close();
+        // Prints each sequential element in array a
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
+    }
+}
---
+++ src/com/hackerRank/Java2DArray.java
@@ -0,0 +1,7 @@
+package com.hackerRank;
+
+/**
+ * created by MehmetBesli on 05.2020
+ */
+public class Java2DArray {
+}

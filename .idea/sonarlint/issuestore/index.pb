7b61d8978d70321899e4cd802f29ee2a18861b78
MehmetBesli <beslimhmht@gmail.com>
2020-05-05 10:19:32 +0300

    Update

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+S
+#src/com/hackerRank/Java1DArray.java,d\4\d475c5f8d083d162abe77238f42aa0588b97d9cf
+S
+#src/com/hackerRank/Java2DArray.java,8\b\8baf85f66d4979975672b3f499bd5b37c1a8e9d1
--- src/com/hackerRank/Java2DArray.java
+++ src/com/hackerRank/Java2DArray.java
@@ -1,7 +1,37 @@
 package com.hackerRank;
 
+import java.util.Scanner;
+
 /**
  * created by MehmetBesli on 05.2020
  */
 public class Java2DArray {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int[][] arr = new int[6][6];
+
+        for (int i = 0; i < 6; i++) {
+            String[] arrRowItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int j = 0; j < 6; j++) {
+                int arrItem = Integer.parseInt(arrRowItems[j]);
+                arr[i][j] = arrItem;
+            }
+        }
+        scanner.close();
+
+        int sum=Integer.MIN_VALUE;
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                int tempSum = arr[i][j] + arr[i][j + 1] + arr[i][j + 2] + arr[i + 1][j + 1] + arr[i + 2][j] + arr[i + 2][j + 1] + arr[i + 2][j + 2];
+                if(tempSum>sum){
+                    sum=tempSum;
+                }
+            }
+        }
+        System.out.println(sum);
+
+    }
 }
